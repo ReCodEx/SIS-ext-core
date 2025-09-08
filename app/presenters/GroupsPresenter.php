b72a41e5bdb7ea9f3504b0f913bda457d81d8b5d
Martin Kruliš <krulis@d3s.mff.cuni.cz>
2025-09-08 19:03:56 +0200

    Implementing bind/unbind endpoints and adding corresponding presenter tests.

--- app/presenters/GroupsPresenter.php
+++ app/presenters/GroupsPresenter.php
@@ -52,9 +52,9 @@ class GroupsPresenter extends BasePresenter
         $term = $event->getTerm()->getYearTermKey();
         $courseCheck = $termCheck = false;
         $group = $groups[$groupId];
-        while ($group !== null && !$courseCheck && !$termCheck) {
-            $courseCheck |= $group->hasCourseAttribute($courseId);
-            $termCheck |= $group->hasTermAttribute($term);
+        while ($group !== null && (!$courseCheck || !$termCheck)) {
+            $courseCheck = $courseCheck || $group->hasCourseAttribute($courseId);
+            $termCheck = $termCheck || $group->hasTermAttribute($term);
             $group = $group->parentGroupId ? ($groups[$group->parentGroupId] ?? null) : null;
         }
 
@@ -158,6 +158,9 @@ class GroupsPresenter extends BasePresenter
 
         $groups = $this->recodexApi->getGroups($this->getCurrentUser());
         $this->isGroupSuitableForEvent($groups, $id, $event); // throws exception if not suitable
+        if ($groups[$id]->organizational) {
+            throw new BadRequestException("Group $id is organizational, so it cannot be bound to a SIS event.");
+        }
 
         if ($groups[$id]->hasGroupAttribute($event->getSisId())) {
             throw new BadRequestException("Group $id is already bound to the selected SIS event.");
@@ -171,7 +174,8 @@ class GroupsPresenter extends BasePresenter
      */
     public function actionBind(string $id, string $eventId)
     {
-        throw new NotImplementedException();
+        $this->recodexApi->addAttribute($id, RecodexGroup::ATTR_GROUP_KEY, $eventId);
+        $this->sendSuccessResponse("OK");
     }
 
     public function checkUnbind(string $id, string $eventId)
@@ -196,7 +200,8 @@ class GroupsPresenter extends BasePresenter
      */
     public function actionUnbind(string $id, string $eventId)
     {
-        throw new NotImplementedException();
+        $this->recodexApi->removeAttribute($id, RecodexGroup::ATTR_GROUP_KEY, $eventId);
+        $this->sendSuccessResponse("OK");
     }
 
     public function checkJoin(string $id)
