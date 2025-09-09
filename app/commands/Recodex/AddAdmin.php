ee92ddafda95ff08f33f970be8fef1e3e95cdd50
Martin Kruliš <krulis@d3s.mff.cuni.cz>
2025-09-09 15:17:57 +0200

    Implementing ReCodEx API calls to add/remove admin to/from a group and corresponding testing CLI commands.

---
+++ app/commands/Recodex/AddAdmin.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Console;
+
+use App\Helpers\RecodexApiHelper;
+use App\Model\Repository\Users;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Add admin into a group as a member.
+ * This command is mainly designed for debugging ReCodEx API integration.
+ */
+class RecodexAddAdmin extends BaseCommand
+{
+    protected static $defaultName = 'recodex:add-admin';
+
+    /**
+     * @var RecodexApiHelper
+     */
+    private $recodexApi;
+
+    /**
+     * @var Users
+     */
+    private $users;
+
+    /**
+     * @param RecodexApiHelper $recodexApi
+     * @param Users $users
+     */
+    public function __construct(RecodexApiHelper $recodexApi, Users $users)
+    {
+        parent::__construct();
+        $this->recodexApi = $recodexApi;
+        $this->users = $users;
+    }
+
+    /**
+     * Register the command.
+     */
+    protected function configure()
+    {
+        $this->setName(self::$defaultName)->setDescription('Add admin into a group as a member.');
+        $this->addArgument('groupId', InputArgument::REQUIRED, 'ID of the group to which the admin will be added.');
+        $this->addArgument('adminId', InputArgument::REQUIRED, 'ID of the admin to be added.');
+    }
+
+    /**
+     * @param InputInterface $input Console input, not used
+     * @param OutputInterface $output Console output for logging
+     * @return int 0 on success, 1 on error
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->input = $input;
+        $this->output = $output;
+
+        $groupId = $input->getArgument('groupId');
+        $adminId = $input->getArgument('adminId');
+        $admin = $this->users->findOrThrow($adminId);
+
+        $token = trim($this->prompt('Auth token: '));
+        if (!$token) {
+            $output->writeln('No token given, terminating...');
+            return Command::SUCCESS;
+        }
+        $this->recodexApi->setAuthToken($token);
+
+        $output->writeln("Adding admin '$adminId' to group '$groupId'...");
+        $this->recodexApi->addAdminToGroup($groupId, $admin);
+
+        return Command::SUCCESS;
+    }
+}
