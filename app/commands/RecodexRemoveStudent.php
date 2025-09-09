aaefa2cb18fef545b6516a0ae86bd1009281bac1
Martin Kruliš <krulis@d3s.mff.cuni.cz>
2025-09-09 13:15:25 +0200

    Implementing add/remove student to/from a group API calls and their testing CLI commands.

---
+++ app/commands/RecodexRemoveStudent.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Console;
+
+use App\Helpers\RecodexApiHelper;
+use App\Model\Repository\Users;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Remove student from a group.
+ * This command is mainly designed for debugging ReCodEx API integration.
+ */
+class RecodexRemoveStudent extends BaseCommand
+{
+    protected static $defaultName = 'recodex:remove-student';
+
+    /**
+     * @var RecodexApiHelper
+     */
+    private $recodexApi;
+
+    /**
+     * @var Users
+     */
+    private $users;
+
+    /**
+     * @param RecodexApiHelper $recodexApi
+     * @param Users $users
+     */
+    public function __construct(RecodexApiHelper $recodexApi, Users $users)
+    {
+        parent::__construct();
+        $this->recodexApi = $recodexApi;
+        $this->users = $users;
+    }
+
+    /**
+     * Register the command.
+     */
+    protected function configure()
+    {
+        $this->setName(self::$defaultName)->setDescription('Remove student from a group.');
+        $this->addArgument(
+            'groupId',
+            InputArgument::REQUIRED,
+            'ID of the group from which the student will be removed.'
+        );
+        $this->addArgument('studentId', InputArgument::REQUIRED, 'ID of the student to be removed.');
+    }
+
+    /**
+     * @param InputInterface $input Console input, not used
+     * @param OutputInterface $output Console output for logging
+     * @return int 0 on success, 1 on error
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->input = $input;
+        $this->output = $output;
+
+        $groupId = $input->getArgument('groupId');
+        $studentId = $input->getArgument('studentId');
+        $student = $this->users->findOrThrow($studentId);
+
+        $token = trim($this->prompt('Auth token: '));
+        if (!$token) {
+            $output->writeln('No token given, terminating...');
+            return Command::SUCCESS;
+        }
+        $this->recodexApi->setAuthToken($token);
+
+        $output->writeln("Removing student '$studentId' from group '$groupId'...");
+        $this->recodexApi->removeStudentFromGroup($groupId, $student);
+
+        return Command::SUCCESS;
+    }
+}
